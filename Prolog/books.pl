5915e8c05f2ae27cefff12f767dccb19d31cda6c
Sandro <layornos@gmail.com>
2019-08-06 22:02:01 +0200

    w3

---
+++ Prolog/books.pl
@@ -0,0 +1,10 @@
+
+wrote(king, it).
+wrote(king, elevation).
+wrote(sanderson, mistborn).
+wrote(sanderson, elantris).
+wrote(sanderson, emperors_soul).
+wrote(child, jack_reacher).
+wrote(wells, time_machine).
+
+wrote(sanderson, X).
