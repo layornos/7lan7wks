8051e1808632108929cc895bcb597c0970979a62
Sandro <layornos@gmail.com>
2019-08-29 21:30:50 +0200

    functions

--- Scala/foldLeft.scala
+++ Scala/foldLeft.scala
@@ -1,3 +1,10 @@
-val inputList: List[String] = List("one", "two", "three", "four")
+def countCharsInListOfStrings(inputList : List[String]): Int = {
+    inputList.foldLeft(0) { (acc, i) => acc + i.length }
+}
 
-println(inputList.foldLeft(0) { (acc, i) => acc + i.length })
+def main() = {
+    val strings: List[String] = List("one", "two", "three", "four")    
+    println(countCharsInListOfStrings(strings))
+}
+
+main()
